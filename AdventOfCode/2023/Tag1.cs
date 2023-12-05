a1c54495ccf87a729343d57ccf877d19c62e130c
Pipeline <Pipeline@auto.com>
2023-12-05 17:44:57 +0100

    EIGHTWO

---
+++ AdventOfCode/2023/Tag1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2023
+{
+    public static class Tag1
+    {
+        public static void Run()
+        {
+            string[] input = Input2023.Tag1.Split("\r\n");
+
+            int result = 0;
+            foreach (string s in input)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    int k;
+                    if(Int32.TryParse(s[i].ToString(), out k))
+                    {
+                        result = result + (k * 10);
+                        break;
+                    };
+                }
+                for (int i = s.Length -1; i >= 0; i--)
+                {
+                    int k;
+                    if (Int32.TryParse(s[i].ToString(), out k))
+                    {
+                        result += k ;
+
+                        break;
+                    };
+                }
+            }
+            Console.WriteLine(result);
+
+        }
+        public static void RunPart2()
+        {
+            string[] input = Input2023.Tag1.Split("\r\n");
+
+            int result = 0;
+            foreach (string s in input)
+            {
+
+
+                string a = "";
+                for(int i = 0;i < s.Length; i++)
+                {
+                    a += s[i];
+                    a = a.Replace("one", "1");
+                    a = a.Replace("two", "2");
+                    a = a.Replace("three", "3");
+                    a = a.Replace("four", "4");
+                    a = a.Replace("five", "5");
+                    a = a.Replace("six", "6");
+                    a = a.Replace("seven", "7");
+                    a = a.Replace("eight", "8");
+                    a = a.Replace("nine", "9");
+
+
+                }
+                string b = "";
+                for (int i = s.Length -1; i >= 0; i--)
+                {
+                    b = s[i] + b;
+                    b = b.Replace("one", "1");
+                    b = b.Replace("two", "2");
+                    b = b.Replace("three", "3");
+                    b = b.Replace("four", "4");
+                    b = b.Replace("five", "5");
+                    b = b.Replace("six", "6");
+                    b = b.Replace("seven", "7");
+                    b = b.Replace("eight", "8");
+                    b = b.Replace("nine", "9");
+
+
+                }
+                if(a != b)
+                {
+                    Console.WriteLine(s);
+                }
+
+                int curr = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    int k;
+                    if (Int32.TryParse(a[i].ToString(), out k))
+                    {
+                        result = result + (k * 10);
+                        curr = curr + (k * 10);
+                        break;
+                    };
+                }
+                for (int i = b.Length - 1; i >= 0; i--)
+                {
+                    int k;
+                    if (Int32.TryParse(b[i].ToString(), out k))
+                    {
+                        result += k;
+
+                        curr += k;
+                        break;
+                    };
+                }
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode;
+using AdventOfCode._2023;
 using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
@@ -6,47 +7,53 @@ using System.Runtime.InteropServices;
 Console.WriteLine("Willkommen zum Weltschönsten Advent Of Code 2022! Anschnallen und dann kanns auch schon los gehen!");
 Console.WriteLine("Beschwerden und Kommentare zu meinem Code sind wie immer nicht erwünscht. Nur Pull requests sind erlaubt! :P \n");
 
-//Day 1
-Day1.Run();
 
-//Day2
-Day2.RunPart1();
-Day2.RunPart2();
-
-//Day3
-Day3.RunPart1();
-Day3.RunPart2();
-
-//Day4
-Day4.Run();
-
-// Day 5
-// TODO: this is not static, because i work with the membervariables directly. Part 2 ruined my logic, that's why this is a bit sloppy in here... Maybe i will fix it.
-//initialize 2 Objects
-Day5 d5Part1 = new Day5();
-Day5 d5Part2 = new Day5();
-
-//execute the Logic
-d5Part1.runPart1();
-d5Part2.runPart2();
+Tag1.Run();
+Tag1.RunPart2();
 
 
-//Day6
-Day6.RunPart1();
-Day6.RunPart2();
+
+////Day 1
+//Day1.Run();
+
+////Day2
+//Day2.RunPart1();
+//Day2.RunPart2();
+
+////Day3
+//Day3.RunPart1();
+//Day3.RunPart2();
+
+////Day4
+//Day4.Run();
+
+//// Day 5
+//// TODO: this is not static, because i work with the membervariables directly. Part 2 ruined my logic, that's why this is a bit sloppy in here... Maybe i will fix it.
+////initialize 2 Objects
+//Day5 d5Part1 = new Day5();
+//Day5 d5Part2 = new Day5();
+
+////execute the Logic
+//d5Part1.runPart1();
+//d5Part2.runPart2();
 
 
-//Day7
-Day7.Run();
-
-//Day8
-//Day8.Run();
-
-Day9.Run();
+////Day6
+//Day6.RunPart1();
+//Day6.RunPart2();
 
 
-Day10.Run();
+////Day7
+//Day7.Run();
 
-Day11.Run();
-//Console.WriteLine("Adding12345 and 778334");
-//Console.WriteLine("Ergebnis: " + StringAdder.Add("12345", "778334"));
+////Day8
+////Day8.Run();
+
+//Day9.Run();
+
+
+//Day10.Run();
+
+//Day11.Run();
+////Console.WriteLine("Adding12345 and 778334");
+////Console.WriteLine("Ergebnis: " + StringAdder.Add("12345", "778334"));
